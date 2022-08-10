d57044241468881b58e1c45b68f92c24ee3b20a9
Leo Fabricio <leofabr3@gmail.com>
2022-08-10 13:47:15 -0300

    Processo Seletivo

--- dio-java-contaBanco/contaBanco/src/ContaTerminal.java
+++ dio-java-contaBanco/contaBanco/src/ContaTerminal.java
@@ -1,21 +1,28 @@
 import java.util.Scanner;
-import java.util.Locale;
+// import java.util.Locale;
 public class ContaTerminal {
     public static void main(String[] args) throws Exception {
         Scanner sc = new Scanner(System.in);
-        Locale locale = new Locale("pt", "BR");
-        int numerodaconta;
-        String agencia, nomeCliente;
-        double saldo;
-        System.out.println("Por favor, digite o número da Agencia: - ");
-        agencia = sc.nextLine();
-        System.out.println("Por favor, digite o número da Conta: - ");
-        numerodaconta = sc.nextInt();
-        System.out.println("Por favor, digite o nome do BENEFICIARIO da conta: - ");
-        nomeCliente = sc.next();
-        System.out.println("Por favor, digite o nome do SALDO da conta: - ");
-        saldo = sc.nextDouble();
-        System.out.println("Olá " + nomeCliente + ", obrigado por criar uma conta em nosso banco, sua agencia é " + agencia + ", conta " + numerodaconta + " e seu saldo de " + saldo + " já está disponível para saque.");
-
+        // Locale locale = new Locale("pt", "BR");
+        // int numerodaconta;
+        // String agencia, nomeCliente;
+        // double saldo;
+        // System.out.println("Por favor, digite o número da Agencia: - ");
+        // agencia = sc.nextLine();
+        // System.out.println("Por favor, digite o número da Conta: - ");
+        // numerodaconta = sc.nextInt();
+        // System.out.println("Por favor, digite o nome do BENEFICIARIO da conta: - ");
+        // nomeCliente = sc.next();
+        // System.out.println("Por favor, digite o nome do SALDO da conta: - ");
+        // saldo = sc.nextDouble();
+        // System.out.println("Olá " + nomeCliente + ", obrigado por criar uma conta em nosso banco, sua agencia é " + agencia + ", conta " + numerodaconta + " e seu saldo de " + saldo + " já está disponível para saque.");
+        String[] nomes = new String[10];
+		
+		for(int i=0 ; i<10 ; i++){
+			nomes[i] = sc.nextLine();
+			if (i == 2) {System.out.println(nomes[i]);};
+			else if ( i == 6) {return nomes[i]};
+			else if ( i == 8) {return nomes[i]};
+        }
     }
 }
