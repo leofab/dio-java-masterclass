26a522f4a0317bc7da92760fe7a20c511f6c6b8b
Leo Fabricio <leofabr3@gmail.com>
2022-08-11 16:00:03 -0300

    cliente getters & setters

--- desafio-dio-banco/Banco/src/user/Cliente.java
+++ desafio-dio-banco/Banco/src/user/Cliente.java
@@ -3,5 +3,38 @@ package user;
 public class Cliente {
   private String nome;
   private String sobrenome;
-  private
+  private String tipoDeConta;
+  private int numeroDaConta;
+
+  public String getNome() {
+    return nome;
+  }
+
+  public String getSobrenome() {
+    return sobrenome;
+  }
+
+  public String getTipoDeConta() {
+    return tipoDeConta;
+  }
+
+  public int getNumeroDaConta() {
+    return numeroDaConta;
+  }
+
+  public void setNome(String nome) {
+    this.nome = nome;
+  }
+
+  public void setSobrenome(String sobrenome) {
+    this.sobrenome = sobrenome;
+  }
+
+  public void setTipoDeConta(String tipoDeConta) {
+    this.tipoDeConta = tipoDeConta;
+  }
+
+  public void setNumeroDaConta(int numeroDaConta) {
+    this.numeroDaConta = numeroDaConta;
+  }
 }
