345b40ea5fd712bee7a417b29af3c7cfe45ac179
Leo Fabricio <leofabr3@gmail.com>
2022-08-11 14:53:32 -0300

    interfaces and implements on iphone implement

---
+++ UML-iphone-projeto/IphoneUML/src/ClassTest.java
@@ -0,0 +1,15 @@
+public class ClassTest {
+  public static void main(String[] args){
+    Iphone iphone = new Iphone();
+
+    iphone.tocar();
+    iphone.atender();
+    iphone.ligar();
+    iphone.iniciarCorreioVoz();
+    iphone.pausar();
+    iphone.selecionarMusica();
+    iphone.adicionarNovaAba();
+    iphone.exibirPagina();
+    iphone.atualizarPagina();
+  }
+}
--- UML-iphone-projeto/IphoneUML/src/Iphone.java
+++ UML-iphone-projeto/IphoneUML/src/Iphone.java
@@ -1,2 +1,53 @@
-public class Iphone {
+public class Iphone implements ReprodutorMusical, AparelhoTelefonico, NavegadorNaInternet {
+  @Override
+  public void tocar() {
+    System.out.println("Tocando musica no Iphone");
+  }
+
+  @Override
+  public void pausar() {
+    System.out.println("Pausando musica no Iphone");
+
+  }
+
+  @Override
+  public void selecionarMusica() {
+    System.out.println("Selecionando musica no Iphone");
+
+  }
+
+  @Override
+  public void ligar() {
+    System.out.println("Ligando do Iphone");
+
+  }
+
+  @Override
+  public void atender() {
+    System.out.println("Atendendo o Iphone");
+
+  }
+
+  @Override
+  public void iniciarCorreioVoz() {
+    System.out.println("Ouvindo o Correio de voz do Iphone");
+
+  }
+
+  @Override
+  public void exibirPagina() {
+    System.out.println("Exibindo Página Web no Iphone");
+  }
+
+  @Override
+  public void adicionarNovaAba() {
+    System.out.println("Adicionando Aba de Página Web no Iphone");
+
+  }
+
+  @Override
+  public void atualizarPagina() {
+    System.out.println("Atualizando Página Web no Iphone");
+
+  }
 }
