7ac0f276fdc9610858d839c072503e027e1ec592
Leo Fabricio <leofabr3@gmail.com>
2022-08-11 15:41:41 -0300

    contas getters & setters

--- desafio-dio-banco/Banco/src/contas/ContaCorrente.java
+++ desafio-dio-banco/Banco/src/contas/ContaCorrente.java
@@ -1,4 +1,32 @@
 package contas;
 
 public class ContaCorrente {
+
+  private int agencia;
+  private int numeroContaC;
+  private double saldo;
+
+  public int getAgencia() {
+    return agencia;
+  }
+
+  public int getNumeroContaC() {
+    return numeroContaC;
+  }
+
+  public double getSaldo() {
+    return saldo;
+  }
+
+  public void setAgencia(int agencia) {
+    this.agencia = agencia;
+  }
+
+  public void setNumeroContaC(int numeroContaC) {
+    this.numeroContaC = numeroContaC;
+  }
+
+  public void setSaldo(double saldo) {
+    this.saldo = saldo;
+  }
 }
--- desafio-dio-banco/Banco/src/contas/ContaPoupanca.java
+++ desafio-dio-banco/Banco/src/contas/ContaPoupanca.java
@@ -1,4 +1,23 @@
 package contas;
 
 public class ContaPoupanca {
+
+  private int agencia;
+  private int numeroContaP;
+
+  public int getAgencia() {
+    return agencia;
+  }
+
+  public int getNumeroContaP() {
+    return numeroContaP;
+  }
+
+  public void setAgencia(int agencia) {
+    this.agencia = agencia;
+  }
+
+  public void setNumeroContaP(int numeroContaP) {
+    this.numeroContaP = numeroContaP;
+  }
 }
