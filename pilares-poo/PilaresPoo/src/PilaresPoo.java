a1d62ce0e071a961f5649b397c94917bdb0eeb12
Leo Fabricio <leofabr3@gmail.com>
2022-08-11 13:43:16 -0300

    metodos implement

--- pilares-poo/PilaresPoo/src/PilaresPoo.java
+++ pilares-poo/PilaresPoo/src/PilaresPoo.java
@@ -1,2 +1,14 @@
 public class PilaresPoo {
+  public void enviarMensagem() {
+    System.out.println("Enviando mensagem");
+  }
+  public void receberMensagem() {
+    System.out.println("Recebendo mensagem");
+  }
+  public void validarConexaoInternet() {
+    System.out.println("Validando se está conectado a internet");
+  }
+  public void salvarHistoricoMensagem() {
+    System.out.println("Salvando o histórico da mensagem");
+  }
 }
