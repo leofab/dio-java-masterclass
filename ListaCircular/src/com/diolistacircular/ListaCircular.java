3b0da7d623d9f5aafc660b0a38df16cb0728bf1b
Leo Fabricio <leofabr3@gmail.com>
2022-08-18 16:58:19 -0300

    ListaCircular get & getNo method implement

--- ListaCircular/src/com/diolistacircular/ListaCircular.java
+++ ListaCircular/src/com/diolistacircular/ListaCircular.java
@@ -5,6 +5,10 @@ public class ListaCircular<T> {
   private No<T> cauda;
   private int tamanhoLista;
 
+  public T get(int index){
+    return this.getNo(index).getConteudo();
+  }
+
   private No<T> getNo(int index){
     if(this.isEmpty())
       throw new IndexOutOfBoundsException("A lista está vazia");
