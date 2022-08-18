aee03210ef875a04f65e14553b31289ca58e6062
Leo Fabricio <leofabr3@gmail.com>
2022-08-18 13:55:33 -0300

    ListaCircula class implement

---
+++ ListaCircular/src/com/diolistacircular/ListaCircular.java
@@ -0,0 +1,33 @@
+package com.diolistacircular;
+
+public class ListaCircular<T> {
+  private No<T> cabeça;
+  private No<T> cauda;
+  private int tamanhoLista;
+
+  private No<T> getNo(int index){
+    if(this.isEmpty())
+      throw new IndexOutOfBoundsException("A lista está vazia");
+
+    if(index == 0){
+      return this.cauda;
+    }
+    No<T> noAuxiliar = this.cauda;
+    for(int i =0; (i < index) && (noAuxiliar != null); i++){
+      noAuxiliar = noAuxiliar.getNoProximo();
+    }
+    return noAuxiliar;
+  }
+
+
+  public boolean isEmpty(){
+    return this.tamanhoLista == 0 ?  true : false;
+  }
+
+
+
+  public int size(){
+    return this.tamanhoLista;
+  }
+
+}
