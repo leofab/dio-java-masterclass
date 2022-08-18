d6b12310bbfb661ea5a5e985c2ea3048ab3ae56b
Leo Fabricio <leofabr3@gmail.com>
2022-08-18 08:20:19 -0300

    NoDuplo implement

--- ListaNoDuplo/src/com/diolistanoduplo/NoDuplo.java
+++ ListaNoDuplo/src/com/diolistanoduplo/NoDuplo.java
@@ -1,4 +1,43 @@
 package com.diolistanoduplo;
 
-public class NoDuplo {
+public class NoDuplo<T> {
+
+  private T conteudo;
+  private NoDuplo<T> noProximo;
+  private NoDuplo<T> noPrevio;
+
+  public NoDuplo(T conteudo){
+    this.conteudo = conteudo;
+  }
+
+  public T getConteudo() {
+    return conteudo;
+  }
+
+  public NoDuplo<T> getNoProximo() {
+    return noProximo;
+  }
+
+  public NoDuplo<T> getNoPrevio() {
+    return noPrevio;
+  }
+
+  public void setConteudo(T conteudo) {
+    this.conteudo = conteudo;
+  }
+
+  public void setNoProximo(NoDuplo<T> noProximo) {
+    this.noProximo = noProximo;
+  }
+
+  public void setNoPrevio(NoDuplo<T> noPrevio) {
+    this.noPrevio = noPrevio;
+  }
+
+  @Override
+  public String toString() {
+    return "NoDuplo{" +
+            "conteudo=" + conteudo +
+            '}';
+  }
 }
