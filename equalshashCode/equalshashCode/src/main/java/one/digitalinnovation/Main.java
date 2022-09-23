704fe0a00082d225fd1fdf6aafd9d9174c5d0a64
Leo Fabricio <leofabr3@gmail.com>
2022-09-23 11:38:58 -0300

    equalshashCode final

--- equalshashCode/equalshashCode/src/main/java/one/digitalinnovation/Main.java
+++ equalshashCode/equalshashCode/src/main/java/one/digitalinnovation/Main.java
@@ -13,6 +13,14 @@ public class Main {
     listaCarros.add(new Carro("Fiat"));
 
     System.out.println(listaCarros.contains(new Carro("Ford")));
+    System.out.println(new Carro("Ford").hashCode());
+    System.out.println(new Carro("Fiat").hashCode());
+
+    Carro carro1 = new Carro("Ford");
+    Carro carro2 = new Carro("Fiat");
+
+
+    System.out.println(carro1.equals(carro2));
 
   }
 }
