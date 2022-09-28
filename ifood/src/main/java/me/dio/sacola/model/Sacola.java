93707e3d44037285b2ca2a2ca22484cbc79a528e
Leo Fabricio <leofabr3@gmail.com>
2022-09-28 12:09:38 -0300

    restaurante class implement

--- ifood/src/main/java/me/dio/sacola/model/Sacola.java
+++ ifood/src/main/java/me/dio/sacola/model/Sacola.java
@@ -6,7 +6,6 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-import me.dio.sacola.enumeration.FormaPagamento;
 
 
 import javax.persistence.*;
