d2bccf6dec0cd84cc799b98b22ab8e5f5f286540
Leo Fabricio <leofabr3@gmail.com>
2022-09-28 14:15:13 -0300

    repositories implement

--- ifood/src/main/java/me/dio/sacola/repository/SacolaRepository.java
+++ ifood/src/main/java/me/dio/sacola/repository/SacolaRepository.java
@@ -1,5 +1,10 @@
 package me.dio.sacola.repository;
 
-public interface SacolaRepository {
+import me.dio.sacola.model.Sacola;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface SacolaRepository extends JpaRepository<Sacola, Long>{
 
 }
