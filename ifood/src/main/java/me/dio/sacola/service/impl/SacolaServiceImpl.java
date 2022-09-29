25ffffd7eda7fd5347ac1a90bbad8a749ddec019
Leo Fabricio <leofabr3@gmail.com>
2022-09-29 16:03:57 -0300

    sacola service impl implement

--- ifood/src/main/java/me/dio/sacola/service/impl/SacolaServiceImpl.java
+++ ifood/src/main/java/me/dio/sacola/service/impl/SacolaServiceImpl.java
@@ -1,11 +1,19 @@
 package me.dio.sacola.service.impl;
 
+import lombok.RequiredArgsConstructor;
+import me.dio.sacola.enumeration.FormaPagamento;
 import me.dio.sacola.model.Item;
 import me.dio.sacola.model.Sacola;
+import me.dio.sacola.repository.SacolaRepository;
 import me.dio.sacola.resource.dto.ItemDto;
 import me.dio.sacola.service.SacolaService;
+import org.springframework.stereotype.Service;
 
+@Service
+@RequiredArgsConstructor
 public class SacolaServiceImpl implements SacolaService {
+
+  private final SacolaRepository sacolaRepository;
   @Override
   public Item incluirItemNaSacola(ItemDto itemDto) {
     return null;
@@ -13,11 +21,28 @@ public class SacolaServiceImpl implements SacolaService {
 
   @Override
   public Sacola verSacola(Long id) {
-    return null;
+    return sacolaRepository.findById(id).orElseThrow(
+            () -> {
+              throw new RuntimeException("Essa sacola não existe");
+            }
+    );
   }
 
   @Override
-  public Sacola fecharSacola(Long id, int formaPagamento) {
-    return null;
+  public Sacola fecharSacola(Long id, int numeroFormaPagamento) {
+    Sacola sacola = verSacola(id);
+
+    if (sacola.getItens().isEmpty()) {
+      throw new RuntimeException("Inclua itens na sacola");
+
+    }
+
+    FormaPagamento formaPagamento = numeroFormaPagamento == 0 ? FormaPagamento.DINHEIRO : FormaPagamento.MAQUINETA;
+
+    sacola.setFormaPagamento(formaPagamento);
+    sacola.setFechada(true);
+
+    return sacolaRepository.save(sacola);
+
   }
 }
